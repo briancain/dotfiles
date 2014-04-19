99f6421cb08b23e64b91b0602a385c7b6a41ee48
Brian Cain <brianccain@gmail.com>
2014-04-19 00:43:07 -0500

    Change git url

--- update-zsh.sh
+++ update-zsh.sh
@@ -9,7 +9,7 @@ echo 'Removing old oh-my-zsh files...'
 rm -rf oh-my-zsh
 
 echo 'Cloning updated version...'
-git clone git@github.com:robbyrussell/oh-my-zsh.git
+git clone https://www.github.com/robbyrussell/oh-my-zsh.git
 
 echo 'Moving theme into oh-my-zsh'
 cp zsh-theme/brian.zsh-theme oh-my-zsh/themes
