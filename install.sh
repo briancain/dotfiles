6e039aaef0f898e801bf50a1564d6979797657f6
Brian Cain <brianccain@gmail.com>
2015-10-01 23:06:14 -0700

    Remove findutils mac package from install

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ set -e
     echo "You are running homebrew."
     echo "Using Homebrew to install packages..."
     brew update
-    declare -a macpackages=('findutils' 'macvim' 'the_silver_searcher')
+    declare -a macpackages=('macvim' 'the_silver_searcher')
     brew install "${packages[@]}" "${macpackages[@]}"
   elif [[ "$OSPACKMAN" == "yum" ]]; then
     echo "You are running yum."
